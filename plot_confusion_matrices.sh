5e2c7798c688c93f8f79ac09d8c9e02259b73267
MQAnh <24520002@gm.uit.edu.vn>
2026-05-24 11:35:42 +0000

    done run XLM-R

--- plot_confusion_matrices.sh
+++ plot_confusion_matrices.sh
@@ -3,6 +3,6 @@
 python plot_confusion_matrices.py \
   --test_file ./data/UIT-ViON_test.csv \
   --lora_pred_file ./topic_phobert_lora/predict_results_topic.txt \
-  --melora_pred_file ./topic_phobert_melora/predict_results_topic.txt \
+  --melora_pred_file ./topic_phobert_Melora/predict_results_topic.txt \
   --label_column label \
   --output_dir ./confusion_matrices
